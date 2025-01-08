748c361229e6057f0b6de6ee8f03a6510a4b6b4d
lanahc <filanahchoi17@gmail.com>
2025-01-08 21:06:38 +0300

    sign uo modified

--- forms/user.php
+++ forms/user.php
@@ -10,23 +10,35 @@ class users{
     $err = $ObjGlob->getMsg('errors');
     ?>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-                        <?php if ($form === 'signup') { ?>
+
                             <div class="mb-3">
                                 <label for="fullname" class="form-label">Full Name:</label>
                                 <input type="text" name="fullname" class="form-control form-control-lg" maxlength="50" id="fullname" placeholder="Enter your name" required>
+                        <?php print (isset($err['nameLetters_err'])) ? "<span class='ivalid'>" . $err['nameLetters_err'] . "</span": '';?> 
                             </div>
                             <div class="mb-3">
                                 <label for="email_address" class="form-label">Email Address:</label>
                                 <input type="email" name="email_address" class="form-control form-control-lg" maxlength="50" id="email_address" placeholder="Enter your email address" required>
+                                <?php print (isset($err['email_format_err'])) ? "<span class='invalid'>" . $err['email_format_err'] . "</span>" : '' ; ?>
+                                <?php print (isset($err['mailExists_err'])) ? "<span class='invalid'>" . $err['mailExists_err'] . "</span>" : '' ; ?>
+                                <?php print (isset($err['mailDomain_err'])) ? "<span class='invalid'>" . $err['mailDomain_err'] . "</span>" : '' ; ?>
                             </div>
-                     <?php   
-
-                }
+                            <div class="mb-3">
+                    <label for="username" class="form-label">Username:</label>
+                    <input type="text" name="username" class="form-control form-control-lg" maxlength="50" id="username" placeholder="Enter your username" <?php print (isset($_SESSION["username"])) ? 'value="'.$_SESSION["username"].'"'  : ''; unset($_SESSION["username"]); ?> >
+                    <?php print (isset($err['usernameExists_err'])) ? "<span class='invalid'>" . $err['usernameExists_err'] . "</span>" : '' ; ?>
+                    <?php print (isset($err['usernameLetters_err'])) ? "<span class='invalid'>" . $err['usernameLetters_err'] . "</span>" : '' ; ?>
+                </div>
+                <button type="submit" name="signup" class="btn btn-primary">Submit</button>
+                     
+<?php
+            }
+            
          }
-    }
-        ?>
+?>    
+    
 
-        
+    <?php    
 
 class TwoFactorAuthForm {
 
