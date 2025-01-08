3d1a67a7a8b7d32f473c895909a21f99ce11c73e
lanahc <filanahchoi17@gmail.com>
2025-01-08 05:18:54 +0300

    user.php

--- user.php
+++ user.php
@@ -214,7 +214,7 @@ class Form {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup Form</title>
+    <title>Form</title>
 </head>
 <body>
 
