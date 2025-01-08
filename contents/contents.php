a720c69c718e72a1b9e0bafbf635621fb7c5691b
lanahc <filanahchoi17@gmail.com>
2025-01-08 20:29:11 +0300

    modified constants

--- contents/contents.php
+++ contents/contents.php
@@ -3,7 +3,17 @@ class contents{
 
     public function heading(){
         ?>
-        
+        div class +"h-100 p-5 text-bg-dark rounded-3">
+        <h2></h2>
+        <p></p>
+    </div>
         <?php
+    }
+    public function about(){
+        ?>
+        <div class="row">
+            <h1></h1>
+            <p></p>
+    <?php
     }
 }
