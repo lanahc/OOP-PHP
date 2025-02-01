91c77941cef4500ae77b13260a6ad7ce5473a810
lanahc <filanahchoi17@gmail.com>
2025-02-01 23:55:43 +0300

    changes

--- OOP/core/init.php
+++ OOP/core/init.php
@@ -19,10 +19,10 @@ $GLOBALS['config'] = array(
 );
 
 spl_autoload_register(function($class) {
-    require_once 'classes/'.$class . '.php';
+    require_once '../classes/'.$class . '.php';
     
 }
 );
-require_once 'functions/sanitize.php';
+require_once '../functions/sanitize.php';
 
 
--- OOP/includes/index.php
+++ OOP/includes/index.php
@@ -1,4 +1,4 @@
 <?php
-require_once 'core/init.php';
+require_once '../core/init.php';
 
-echo Config::get('mysql/host');//  '127.0.0.1
+echo Config::get('mysql/host');//  '127.0.0.1'
