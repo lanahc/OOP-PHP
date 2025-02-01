0ec1db96efec97ac2ce3ec87c3ffa232855a45ab
lanahc <filanahchoi17@gmail.com>
2025-02-01 23:46:53 +0300

    changes

--- OOP/classes/config.php
+++ OOP/classes/config.php
@@ -3,6 +3,9 @@ class Config{
     public static function get($path = null) {
         if($path) {
             $config = $GLOBALS['config'];
+            $path = explode('/', $path);
+
+            print_r($path);
         }
     }
 }
