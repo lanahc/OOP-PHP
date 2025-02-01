624f54e693b5920599837396ad076cab3ddc7de2
lanahc <filanahchoi17@gmail.com>
2025-02-02 00:42:13 +0300

    db

--- OOP/classes/DB.php
+++ OOP/classes/DB.php
@@ -6,4 +6,12 @@ class DB{
             $_error = false, 
             $_results,
             $_count = 0;
+
+    private function _construct() {
+        try{
+    $this->_pdo = new PDO ('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'),Config::get('mysql/password'));
+        } catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
 }
--- OOP/includes/index.php
+++ OOP/includes/index.php
@@ -1,9 +1,3 @@
 <?php
 require_once '../core/init.php';
 
-$users = DB::getInstance()->query('SELECT username FROM users');
-if($users->count()) {
-    foreach($users as $user) {
-        echo $user->username;
-    }
-}
