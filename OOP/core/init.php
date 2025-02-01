6f021ffd87fdcc62ecfda6e70a2af91b8dd693dc
lanahc <filanahchoi17@gmail.com>
2025-02-01 22:58:28 +0300

    code

--- OOP/core/init.php
+++ OOP/core/init.php
@@ -17,3 +17,12 @@ $GLOBALS['config'] = array(
     )
 
 );
+
+spl_autoload_register(function($class) {
+    require_once 'classes/'.$class . '.php';
+    
+}
+);
+require_once 'functions/sanitize.php';
+
+
